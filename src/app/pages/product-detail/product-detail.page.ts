d8e8498804ac56e1f85f9c2f09409cfa7ae3bb64
Alexander Rodriguez <32604358+adragonrc@users.noreply.github.com>
2025-07-09 18:22:33 -0500

    Add product detail page and routing

---
+++ src/app/pages/product-detail/product-detail.page.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule, AlertController, ToastController } from '@ionic/angular';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ProductsService, Product } from '../../services/products.service';
+import { CartService } from '../../services/cart.service';
+
+@Component({
+  standalone: true,
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.page.html',
+  styleUrls: ['./product-detail.page.scss'],
+  imports: [IonicModule, CommonModule, RouterModule],
+})
+export class ProductDetailPage implements OnInit {
+  private productsService = inject(ProductsService);
+  private route = inject(ActivatedRoute);
+  private cartService = inject(CartService);
+  private alertCtrl = inject(AlertController);
+  private toastCtrl = inject(ToastController);
+
+  product$!: Observable<Product | undefined>;
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.product$ = this.productsService.getProductById(id);
+    }
+  }
+
+  async addToCart(product: Product) {
+    await this.cartService.addToCart(product);
+    const toast = await this.toastCtrl.create({
+      message: 'Producto agregado al carrito',
+      duration: 1500,
+    });
+    toast.present();
+  }
+
+  async buyNow(product: Product) {
+    await this.cartService.removeItem(product.id!);
+    const alert = await this.alertCtrl.create({
+      header: 'Compra',
+      message: 'Compra realizada con éxito',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+}
